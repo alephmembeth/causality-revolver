b29e56eb5f5f365f26e56f72a39da6faf4caed46
Jan Romann <jan.romann@uni-bremen.de>
2022-12-31 22:42:11 +0100

    Reformat R script

--- analysis/causality_revolver.R
+++ analysis/causality_revolver.R
@@ -6,8 +6,10 @@ library(xtable)
 
 # data import
 causality_revolver <- haven::read_dta("causality_revolver.dta")
-sytsma_livengood_797 <- read.delim("sytsma_livengood_797.txt", comment.char="#")
-sytsma_livengood_798 <- read.delim("sytsma_livengood_798.txt", comment.char="#")
+sytsma_livengood_797 <-
+  read.delim("sytsma_livengood_797.txt", comment.char = "#")
+sytsma_livengood_798 <-
+  read.delim("sytsma_livengood_798.txt", comment.char = "#")
 
 
 # vector of the neutral value 4
@@ -30,40 +32,40 @@ cliff.delta(causality_revolver$fh, neutral_rev)
 cliff.delta(causality_revolver$gh, neutral_rev)
 
 revolver_all <- causality_revolver %>%
-   dplyr::select(
-      id,
-      ab,
-      ac,
-      ad,
-      ae,
-      af,
-      ag,
-      ah,
-      bc,
-      bd,
-      be,
-      bf,
-      bg,
-      bh,
-      cd,
-      ce,
-      cf,
-      cg,
-      ch,
-      de,
-      df,
-      dg,
-      dh,
-      ef,
-      eg,
-      eh,
-      fg,
-      fh,
-      gh
-   ) %>%
-   tidyr::pivot_longer(-id, names_to = "case", values_to = "value") %>%
-   mutate(
-      case = recode(
+  dplyr::select(
+    id,
+    ab,
+    ac,
+    ad,
+    ae,
+    af,
+    ag,
+    ah,
+    bc,
+    bd,
+    be,
+    bf,
+    bg,
+    bh,
+    cd,
+    ce,
+    cf,
+    cg,
+    ch,
+    de,
+    df,
+    dg,
+    dh,
+    ef,
+    eg,
+    eh,
+    fg,
+    fh,
+    gh
+  ) %>%
+  tidyr::pivot_longer(-id, names_to = "case", values_to = "value") %>%
+  mutate(
+    case = recode(
       case,
       `ab` = "A/B",
       `ac` = "A/C",
@@ -93,93 +95,112 @@ revolver_all <- causality_revolver %>%
       `fg` = "F/G",
       `fh` = "F/H",
       `gh` = "G/H",
-      )
-   ) %>% mutate(own_study = TRUE)
+    )
+  ) %>% mutate(own_study = TRUE)
 
 original_revolver_data <- sytsma_livengood_797 %>%
-   rename("A/H" = Trent,
-          "B/H" = Hammer,
-          "D/H" = Powder,
-          "F/H" = Bullet,
-   ) %>% mutate(id = row_number() + 52,
-          "E/H" = `D/H`,
-          "G/H" = `F/H`,) %>% 
-   tidyr::pivot_longer(-id, names_to = "case", values_to = "value") %>%
-   mutate(own_study = FALSE)
+  rename(
+    "A/H" = Trent,
+    "B/H" = Hammer,
+    "D/H" = Powder,
+    "F/H" = Bullet,
+  ) %>% mutate(id = row_number() + 52,
+               "E/H" = `D/H`,
+               "G/H" = `F/H`,
+  ) %>%
+  tidyr::pivot_longer(-id, names_to = "case", values_to = "value") %>%
+  mutate(own_study = FALSE)
 
 revolver_all %>%
-   bind_rows(original_revolver_data) %>%
-   filter(case %in% c("A/H", "B/H", "D/H", "E/H", "F/H", "G/H")) %>%
-   mutate(
-      case = recode(
-         case,
-         `A/H` = "1 vs. A/H",
-         `B/H` = "2 vs. B/H",
-         `D/H` = "3 vs. D/H",
-         `E/H` = "3 vs. E/H",
-         `F/H` = "4 vs. F/H",
-         `G/H` = "4 vs. G/H",
-      )
-   ) %>%
-   group_by(own_study, case, value) %>%
-   summarise(n = n()) %>%
-   mutate(freq = n / sum(n)) %>%
-   ungroup() %>%
-   complete(own_study, case, value, fill = list(n = 0, freq = 0)) %>%
-   ggplot2::ggplot(ggplot2::aes(x = value, y= freq, fill = own_study)) +
-   ggplot2::geom_bar(color="black", position = "dodge", stat="identity") +
-   facet_wrap(~case, scales = "free") +
-   scale_fill_manual(name ="", labels = c("Livengood & Sytsma", "Our Study"), values=c("#ffffff", "#000000")) +
-   scale_x_continuous(breaks = seq(1, 7, by = 1)) +
-   scale_y_continuous(limits = c(0, 0.9), breaks = seq(0, 0.8, by = 0.2)) +
-   xlab("Response") +
-   ylab("Relative Frequency") +
-   theme_bw() +
-   theme(legend.position="top")
+  bind_rows(original_revolver_data) %>%
+  filter(case %in% c("A/H", "B/H", "D/H", "E/H", "F/H", "G/H")) %>%
+  mutate(
+    case = recode(
+      case,
+      `A/H` = "1 vs. A/H",
+      `B/H` = "2 vs. B/H",
+      `D/H` = "3 vs. D/H",
+      `E/H` = "3 vs. E/H",
+      `F/H` = "4 vs. F/H",
+      `G/H` = "4 vs. G/H",
+    )
+  ) %>%
+  group_by(own_study, case, value) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  ungroup() %>%
+  complete(own_study, case, value, fill = list(n = 0, freq = 0)) %>%
+  ggplot2::ggplot(ggplot2::aes(x = value, y = freq, fill = own_study)) +
+  ggplot2::geom_bar(color = "black",
+                    position = "dodge",
+                    stat = "identity") +
+  facet_wrap( ~ case, scales = "free") +
+  scale_fill_manual(
+    name = "",
+    labels = c("Livengood & Sytsma", "Our Study"),
+    values = c("#ffffff", "#000000")
+  ) +
+  scale_x_continuous(breaks = seq(1, 7, by = 1)) +
+  scale_y_continuous(limits = c(0, 0.9), breaks = seq(0, 0.8, by = 0.2)) +
+  xlab("Response") +
+  ylab("Relative Frequency") +
+  theme_bw() +
+  theme(legend.position = "top")
 
-ggsave("../preprint/comparison.pdf", width = 14.43, height=11.29, units = "cm")
+ggsave(
+  "../preprint/comparison.pdf",
+  width = 14.43,
+  height = 11.29,
+  units = "cm"
+)
 
 # p values
 revolver_all %>%
-   dplyr::group_by(case) %>%
-   dplyr::summarise(value = list(value)) %>%
-   dplyr::group_by(case) %>%
-   dplyr::mutate(
-      v_value = wilcox.test(unlist(value), mu = 4)$statistic,
-      p_value = wilcox.test(unlist(value), mu = 4)$p.value
-   ) %>%
-   select(-value) %>%
-   dplyr::ungroup() %>%
-   mutate(p_value_adjusted = stats::p.adjust(p_value, method = "fdr")) %>%
-   mutate(
-      p_value2 = case_when(
-         p_value < 0.001 ~ paste0("$< 0.001$\\sym{***}"),
-         p_value < 0.01 ~ paste0("$\\phantom{< }", round(p_value, 3), "$\\sym{**\\phantom{*}}"),
-         p_value < 0.05 ~ paste0(round(p_value, 3), "$\\sym{*\\phantom{**}}"),
-         TRUE ~ formatC(p_value, digits = 3)
+  dplyr::group_by(case) %>%
+  dplyr::summarise(value = list(value)) %>%
+  dplyr::group_by(case) %>%
+  dplyr::mutate(
+    v_value = wilcox.test(unlist(value), mu = 4)$statistic,
+    p_value = wilcox.test(unlist(value), mu = 4)$p.value
+  ) %>%
+  select(-value) %>%
+  dplyr::ungroup() %>%
+  mutate(p_value_adjusted = stats::p.adjust(p_value, method = "fdr")) %>%
+  mutate(
+    p_value2 = case_when(
+      p_value < 0.001 ~ paste0("$< 0.001$\\sym{***}"),
+      p_value < 0.01 ~ paste0("$\\phantom{< }", round(p_value, 3), "$\\sym{**\\phantom{*}}"),
+      p_value < 0.05 ~ paste0(round(p_value, 3), "$\\sym{*\\phantom{**}}"),
+      TRUE ~ formatC(p_value, digits = 3)
+    ),
+    p_value_adjusted2 = case_when(
+      p_value_adjusted < 0.001 ~ paste0("$< 0.001$\\sym{***}"),
+      p_value_adjusted < 0.01 ~ paste0(
+        "$\\phantom{< }",
+        round(p_value_adjusted, 3),
+        "$\\sym{**\\phantom{*}}"
       ),
-      p_value_adjusted2 = case_when(
-         p_value_adjusted < 0.001 ~ paste0("$< 0.001$\\sym{***}"),
-         p_value_adjusted < 0.01 ~ paste0("$\\phantom{< }", round(p_value_adjusted, 3), "$\\sym{**\\phantom{*}}"),
-         p_value_adjusted < 0.05 ~ paste0("$\\phantom{< }", round(p_value_adjusted, 3), "$\\sym{*\\phantom{**}}"),
-         TRUE ~ formatC(p_value_adjusted, digits = 3)
+      p_value_adjusted < 0.05 ~ paste0(
+        "$\\phantom{< }",
+        round(p_value_adjusted, 3),
+        "$\\sym{*\\phantom{**}}"
       ),
-   ) %>%
-   select(-p_value_adjusted, -p_value,
-      -p_value2
-   ) %>%
-   rename(
-      "Case" = case,
-      "$V$" = v_value,
-      "Adjusted $p$-value" = p_value_adjusted2,
-   ) -> table_data
+      TRUE ~ formatC(p_value_adjusted, digits = 3)
+    ),
+  ) %>%
+  select(-p_value_adjusted,-p_value,-p_value2) %>%
+  rename("Case" = case,
+         "$V$" = v_value,
+         "Adjusted $p$-value" = p_value_adjusted2,) -> table_data
 
 print(
-   xtable(table_data, type = "latex"),
-   floating = FALSE,
-   latex.environments = NULL,
-   booktabs = TRUE,
-   file = "../preprint/wilcoxon2.tex",
-   sanitize.text.function=function(x){x},
-   include.rownames = FALSE
+  xtable(table_data, type = "latex"),
+  floating = FALSE,
+  latex.environments = NULL,
+  booktabs = TRUE,
+  file = "../preprint/wilcoxon2.tex",
+  sanitize.text.function = function(x) {
+    x
+  },
+  include.rownames = FALSE
 )
